1ca925e640eda3557f435725a51cea38aaf9decf
郑伟 <511927343@qq.com>
2023-03-17 22:52:04 +0800

    修改

--- PythonTest/Game.py
+++ PythonTest/Game.py
@@ -1,16 +1,19 @@
 import random
 
-count = 5
+count = countNumber = 5
 
+randomNumber = random.randint(1, 10)
 while count > 0:
     temp = input("请输入 1-10 之间的数，猜猜你输入的数字是否和随机数相等：")
     guessNumber = int(temp)
-    randomNumber = random.randint(1, 10)
-    if guessNumber != randomNumber:
-        print(f"猜错啦~,正确数字为：{randomNumber}")
-    else:
+    if guessNumber == randomNumber:
         print("恭喜你，猜对了~")
         break
+    else:
+        if guessNumber > randomNumber:
+            print("大啦")
+        else:
+            print("小了")
     count = count - 1
     if count == 0:
-        print(f"已经猜了{count}次，没机会了")
+        print(f"已经猜了{countNumber}次，没机会了，正确答案为：{randomNumber}")
