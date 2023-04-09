5a966d32268b86d1126c2575e8ba403762285572
郑伟 <511927343@qq.com>
2023-04-09 22:19:08 +0800

    画图相关

---
+++ PythonTest/python_repos_visual.py
@@ -0,0 +1,36 @@
+import requests
+from plotly import offline
+
+# 执行API调用并存储响应。
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# 处理结果。
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# 可视化。
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6
+}]
+my_layout = {
+    'title': 'GitHub上最受欢迎的Python项目',
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Stars'},
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
