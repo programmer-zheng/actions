81e467cdedb083694ad98b75a4efb8fa0a96f92a
郑伟 <zhengwei@ftrans.cn>
2023-09-15 17:00:07 +0800

    视频合并

--- DotNet/VideoMerge/Program.cs
+++ DotNet/VideoMerge/Program.cs
@@ -1,3 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true);
+        var config = configurationBuilder.Build();
+        var videoBaseDirectory = config.GetValue<string>("BaseDirectory");
+        if (Directory.Exists(videoBaseDirectory))
+        {
+            var dateDirectories = Directory.GetDirectories(videoBaseDirectory);
+            foreach (var dir in dateDirectories)
+            {
+                // 获取目录名称，后续用作合并视频文件名
+                var directoryName = new DirectoryInfo(dir).Name;
+                var videoFiles = Directory.GetFiles(dir, "*", SearchOption.TopDirectoryOnly);
+                var convertFile = Path.Combine(videoBaseDirectory, directoryName, $"{directoryName}.txt");
+                var outputFile = Path.Combine(videoBaseDirectory, $"{directoryName}.mp4");
+                using (StreamWriter writer = new StreamWriter(convertFile))
+                {
+                    foreach (var file in videoFiles)
+                    {
+                        var videoFileName = new FileInfo(file).Name;
+                        writer.WriteLine($"file {videoFileName}");
+                    }
+                }
+
+                // 拼接转换命令
+                Console.WriteLine($"ffmpeg -safe 0 -f concat -i {convertFile} -c:v copy -c:a aac {outputFile}");
+            }
+        }
+    }
+}
