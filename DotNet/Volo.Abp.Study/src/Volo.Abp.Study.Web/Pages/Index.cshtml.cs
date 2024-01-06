e1f5278c783daf82cb7792e4abf5a7307fb5565d
郑伟 <511927343@qq.com>
2024-01-06 22:00:21 +0800

    默认首页也需要登录访问

--- DotNet/Volo.Abp.Study/src/Volo.Abp.Study.Web/Pages/Index.cshtml.cs
+++ DotNet/Volo.Abp.Study/src/Volo.Abp.Study.Web/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿namespace Volo.Abp.Study.Web.Pages;
+﻿using Microsoft.AspNetCore.Authorization;
 
+namespace Volo.Abp.Study.Web.Pages;
+
+[Authorize]
 public class IndexModel : StudyPageModel
 {
     public void OnGet()
