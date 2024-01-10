1f74b0494b4f0c451e4e58cf92ed336525b68769
郑伟 <511927343@qq.com>
2024-01-10 19:15:10 +0800

    添加门店管理实体

---
+++ DotNet/Volo.Abp.Study/src/Volo.Abp.Study.Domain.Shared/FlatRouteDeviceTypeEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Volo.Abp.Study;
+
+public enum FlatRouteDeviceTypeEnum
+{
+    Mikrotik = 1,
+    
+    /// <summary>
+    /// 爱快
+    /// </summary>
+    IKuai = 2
+}
---
+++ DotNet/Volo.Abp.Study/src/Volo.Abp.Study.Domain/FlatManage/FlatInfoEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Volo.Abp.Domain.Entities;
+
+namespace Volo.Abp.Study.FlatManage;
+
+public class FlatInfoEntity : Entity<Guid>, ISoftDelete
+{
+    [Required]
+    public required string FlatName { get; set; }
+
+    public string? EnName { get; set; }
+
+    public FlatRouteDeviceTypeEnum DeviceType { get; set; } = FlatRouteDeviceTypeEnum.Mikrotik;
+
+    public string? ConfigStr { get; set; }
+
+    public bool IsDeleted { get; } = false;
+}
