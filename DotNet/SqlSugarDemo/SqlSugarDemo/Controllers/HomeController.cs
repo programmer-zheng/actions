3348a319452470c27b096cd833b2575b70ad4dc5
郑伟 <511927343@qq.com>
2024-04-07 11:33:37 +0800

    测试sqlsugar的storageable方法

--- DotNet/SqlSugarDemo/SqlSugarDemo/Controllers/HomeController.cs
+++ DotNet/SqlSugarDemo/SqlSugarDemo/Controllers/HomeController.cs
@@ -15,15 +15,31 @@ public class HomeController : Controller
     {
         _logger = logger;
         this.db = db;
+        Task.Run(async () => { await InitData(); });
+    }
+
+    async Task InitData()
+    {
+        var studentList = new List<Student>
+        {
+            new Student() { Id = 1, Name = "张三" },
+            new Student() { Id = 2, Name = "李四" },
+            new Student() { Id = 3, Name = "王五" },
+        };
+
+        var books = new List<Book>
+        {
+            new() { Id = 1, SID = 1, BookName = "格林童话" },
+            new() { Id = 2, SID = 1, BookName = "寓言故事" },
+            new() { Id = 3, SID = 2, BookName = "C# 指南" },
+        };
+
+        await db.Storageable(studentList).ExecuteCommandAsync();
+        await db.Storageable(books).ExecuteCommandAsync();
     }
 
     public async Task<IActionResult> Index()
     {
-        await db.Insertable<Student>(new List<Student>()
-        {
-            new Student() { Name = "test111" },
-            new Student() { Name = "test222" },
-        }).ExecuteCommandAsync();
         var students = db.Queryable<Student>().Includes(t => t.Books).ToList();
         return Json(students);
     }
