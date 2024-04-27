399fc7a80773d14675f4eecb33a2037f3070760f
郑伟 <511927343@qq.com>
2024-04-27 10:25:05 +0800

    init

---
+++ DotNet/CustomTemplateSearch/CustomTemplateSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomTemplateSearch.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
