c0175058537411b2bece2aa6476c3dab97b6c549
zheng <511927343@qq.com>
2025-03-25 22:00:25 +0800

    测试新增接口

---
+++ DotNet/Furion.Demo/Furion.Demo.Application/System/Services/FurionDemoApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furion.Demo.Application.System.Services;
+
+public class FurionDemoApi : IDynamicApiController
+{
+
+    /// <summary>
+    /// 打招呼
+    /// </summary>
+    /// <returns></returns>
+    [ActionName("SayHello")]
+    public string SayHello()
+    {
+        return "Hello Furion";
+    }
+}
