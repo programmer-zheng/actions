05d66d28ac73b86e219736ace120146e623683d4
zheng <511927343@qq.com>
2025-05-20 15:32:16 +0800

    更换实体位置

--- DotNet/Furion.Demo/Furion.Demo.Core/Entity/PointDataEntity.cs
+++ DotNet/Furion.Demo/Furion.Demo.Core/Entity/PointDataEntity.cs
@@ -64,26 +64,6 @@ public class PointDataEntity : ITdPrimaryKey
     public double? MinValue { get; set; }
 }
 
-/// <summary>
-/// MySQL数据库中的表
-/// </summary>
-[SugarTable("Point_Data")]
-[TraditionDataTable]
-[Tenant(Consts.MainConfigId)]
-public class PointEntity : BaseEntity
-{
-    [SugarColumn(IsPrimaryKey = true)]
-    public long Id { get; set; }
-
-    public string SNO { get; set; }
-
-    public string PointType { get; set; }
-
-    public string PointNumber { get; set; }
-
-    public double PointValue { get; set; }
-}
-
 public abstract class BaseEntity : ISoftDelete
 {
     public long Id { get; set; }
---
+++ DotNet/Furion.Demo/Furion.Demo.Core/Entity/PointEntity.cs
@@ -0,0 +1,23 @@
+﻿using SqlSugar;
+
+namespace Furion.Demo.Core;
+
+/// <summary>
+/// MySQL数据库中的表
+/// </summary>
+[SugarTable("Point_Data")]
+[TraditionDataTable]
+[Tenant(Consts.MainConfigId)]
+public class PointEntity : BaseEntity
+{
+    [SugarColumn(IsPrimaryKey = true)]
+    public long Id { get; set; }
+
+    public string SNO { get; set; }
+
+    public string PointType { get; set; }
+
+    public string PointNumber { get; set; }
+
+    public double PointValue { get; set; }
+}
